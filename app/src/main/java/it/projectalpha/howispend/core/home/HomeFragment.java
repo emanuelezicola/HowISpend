aebea6c4ecbabcdbbc0271de10b05e1142dcad69
emanuele zicola <emanuelezicola95@gmail.com>
2019-10-24 18:31:35 +0200

    implementazione bottom navigation cerca

--- app/src/main/java/it/projectalpha/howispend/core/home/HomeFragment.java
+++ app/src/main/java/it/projectalpha/howispend/core/home/HomeFragment.java
@@ -12,11 +12,14 @@ import it.projectalpha.howispend.R;
 
 public class HomeFragment extends Fragment {
 
+    private View view;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.home_fragment, null);
+        view = inflater.inflate(R.layout.home_fragment, null);
+        return view;
     }
 
 }
